6fc79fb6c30aba181095641f499f6e5a5c7e903f
swalya <swalya@safaricom.co.ke>
2024-03-28 12:27:19 +0300

    changes

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+ 
+const nextConfig = {
+  reactStrictMode: true,
+ 
+  webpack: (config, { isServer }) => {
+    config.module.rules.push({
+      test: /\.(graphql|gql)/,
+      exclude: /node_modules/,
+      loader: "graphql-tag/loader",
+    });
+ 
+    return config;
+  },
+};
+ 
+module.exports = nextConfig;
+ 
